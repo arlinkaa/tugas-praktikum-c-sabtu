d949a6ba52bf4fc9646947a49fddd3f2d3eea0be
arlinka <147239998+arlinkaa@users.noreply.github.com>
2023-10-11 10:14:39 +0700

    Add files via upload

---
+++ Tugas Pertemuan 1/Tugas 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int nim = 233149;
+	char* nama = "Arlinka";
+	char* jurusan = "teknik iformatika";
+	
+	printf("nim : %d \n", nim);
+	printf("nama : %s \n", nama);
+	printf("jurusan : %s", jurusan);
+	return 0;
+}
